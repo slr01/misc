ae7661dc289aa4da511cde9d432c7a63328e079e
Sam <sam.rodriguez1@gmail.com>
2022-12-06 10:52:57 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,6 +9,11 @@ sudo apt install conky -y
 sudo mv /etc/conky/conky.conf /etc/conky/conky.conf.old
 sudo wget -P /etc/conky https://raw.githubusercontent.com/slr01/misc/main/conky.conf
 
+###Remove Libre Office and Thunderbird
+sudo apt remove --purge libreoffice* thunderbird* -y
+sudo apt clean
+sudo apt autoremove
+
 ###Work around for the bug related to the HSUART DMA kernel module on Cherry Trail CPUs
 echo '\nblacklist dw_dmac_core\ninstall dw_dmac /bin/true\ninstall dw_dmac_core /bin/true' >> /etc/modprobe.d/blacklist.conf
 sudo update-initramfs -u
