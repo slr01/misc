a49a24e9ed6d5e9303cb7a6d60ca4b2b26328775
Sam <sam.rodriguez1@gmail.com>
2022-12-13 13:01:28 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 ###Script to configure a fresh install of Xubuntu
-wget -P ~/Pictures https://github.com/slr01/misc/raw/main/zen.jpg
 sudo apt install curl
 curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
 sudo apt install speedtest
@@ -8,7 +7,6 @@ sudo apt install iperf3 -y
 sudo apt install conky -y
 sudo mv /etc/conky/conky.conf /etc/conky/conky.conf.old
 sudo wget -P /etc/conky https://raw.githubusercontent.com/slr01/misc/main/conky.conf
-sudo apt install lightdm-gtk-greeter-settings
 
 ###Remove Libre Office and Thunderbird
 sudo apt remove --purge libreoffice* thunderbird* -y
@@ -16,5 +14,8 @@ sudo apt clean
 sudo apt autoremove
 
 ###Work around for the bug related to the HSUART DMA kernel module on Cherry Trail CPUs
-echo '\nblacklist dw_dmac_core\ninstall dw_dmac /bin/true\ninstall dw_dmac_core /bin/true' >> /etc/modprobe.d/blacklist.conf
+sudo echo '\nblacklist dw_dmac_core\ninstall dw_dmac /bin/true\ninstall dw_dmac_core /bin/true' >> /etc/modprobe.d/blacklist.conf
 sudo update-initramfs -u
+
+###For Use As A Headless Display
+sudo echo '\nvkms' >> /etc/modules-load.d/modules.conf
